b38b653c594bba77bd4f41fa5f8bf4025257ba1c
YUapisit <aps.apisit250@gmail.com>
2023-05-07 23:01:52 +0700

    minimal build 2

--- comsci_website/src/components/MinimalComponents/MinimalAlumniComponent.vue
+++ comsci_website/src/components/MinimalComponents/MinimalAlumniComponent.vue
@@ -2,41 +2,51 @@
     <div class="container-fluid p-1 pt-2 pt-md-0 p-md-0 pt-md-5 p-lg-3 pt-lg-5">
         <h2>รุ่นพี่ศิษย์เก่า สาขาวิทยาการคอมพิวเตอร์</h2>
         <hr>
-        <Flicking ref="flicking2" :options="{ align: 'center', circular: true }" :plugins="plugins">
-            <div class="alumni">
+        <Flicking ref="flicking2" :options="options" :plugins="plugins" class="p-3">
+            <div class="alumni panel">
                 <img src="@/assets/img/Alumni/alumni(1).jpg" alt="" srcset="" class="img-fluid">
             </div>
-            <div class="alumni">
+            <div class="alumni panel">
                 <img src="@/assets/img/Alumni/alumni(2).jpg" alt="" srcset="" class="img-fluid">
             </div>
-            <div class="alumni">
+            <div class="alumni panel">
                 <img src="@/assets/img/Alumni/alumni(3).jpg" alt="" srcset="" class="img-fluid">
             </div>
-            <div class="alumni">
+            <div class="alumni panel">
                 <img src="@/assets/img/Alumni/alumni(4).jpg" alt="" srcset="" class="img-fluid">
             </div>
-            <div class="alumni">
+            <div class="alumni panel">
                 <img src="@/assets/img/Alumni/alumni(5).jpg" alt="" srcset="" class="img-fluid">
             </div>
-            <div class="alumni">
+            <div class="alumni panel">
                 <img src="@/assets/img/Alumni/alumni(6).jpg" alt="" srcset="" class="img-fluid">
             </div>
-            <div class="alumni">
+            <div class="alumni panel">
                 <img src="@/assets/img/Alumni/alumni(7).jpg" alt="" srcset="" class="img-fluid">
             </div>
-            <div class="alumni">
+            <div class="alumni panel">
                 <img src="@/assets/img/Alumni/alumni(8).jpg" alt="" srcset="" class="img-fluid">
             </div>
-            <div class="alumni">
+            <div class="alumni panel">
                 <img src="@/assets/img/Alumni/alumni(9).jpg" alt="" srcset="" class="img-fluid">
             </div>
+            <template #viewport>
+                <div class="flicking-pagination"></div>
+            </template>
+
         </Flicking>
+        <div class="flicking-pagination"></div>
     </div>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
 import Flicking from "@egjs/vue3-flicking";
+
+// import { AutoPlay } from "@egjs/flicking-plugins";
+
+import { Perspective, AutoPlay, Pagination } from "@egjs/flicking-plugins";
+
 
 
 export default defineComponent({
@@ -46,6 +56,22 @@ export default defineComponent({
     },
     data() {
         return {
+            options: {
+                align: 'center',
+                circular: true,
+                // inputType: ["touch", "mouse"],
+                // moveType: ["strict", { count: 1 }]
+
+            },
+
+            plugins: [
+                new Perspective({ rotate: 0.5 }),
+                new AutoPlay({ duration: 2500, direction: "NEXT", stopOnHover: true }),
+                new Pagination({ type: 'scroll' })
+
+            ]
+
+
 
 
         }
--- comsci_website/src/main.js
+++ comsci_website/src/main.js
@@ -5,13 +5,13 @@ import Flicking from "@egjs/vue3-flicking/dist/flicking.esm";
 import "@egjs/vue3-flicking/dist/flicking.css";
 import "@egjs/vue3-flicking/dist/flicking-inline.css";
 import "@egjs/flicking-plugins/dist/flicking-plugins.css";
-
+import "@egjs/flicking-plugins/dist/pagination.css";
 
 import './assets/css/style.css'
 // import './assets/css/owl.carousel.css'
 // import './assets/css/owl.theme.css'
 // import './assets/css/owl.transitions.css'
-import './assets/css/ResponsiveCSS/responsive.css'
+// import './assets/css/ResponsiveCSS/responsive.css'
 import router from './router'
 
 
